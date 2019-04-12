9ae9dc70b83db04e5b41bf2f39cd0141c59f8f14
GuoJiafeng <18611781163@163.com>
2019-04-12 11:13:38 +0800

    Github 加速

--- install.sh
+++ install.sh
@@ -7,7 +7,14 @@ config_java(){
 
 service iptables stop
 
+echo "配置Github加速"
+config_github
+echo "配置Github结束"
+
+sleep 1
+
 echo "开始下载项目"
+sleep 1
 
 rm -rf ShortVideoProject.jar
 
@@ -15,6 +22,7 @@ wget https://github.com/GuoJiafeng/ShortVideoProject/releases/download/1.1/Short
 
 echo "项目下载完成"
 
+sleep 1
 
 read -p "请输入项目的WEB端口号(默认1211):" webport
 
@@ -28,15 +36,19 @@ read -p "请输入Enter确认启动项目"
 nohup java -jar ShortVideoProject.jar --spring.datasource.password=${mysqlpass} --server.port=${webport} --server.context-path=/${contextpath} &
 
 echo "开始下载Runner.sh"
+sleep 1
 
 wget  https://raw.githubusercontent.com/GuoJiafeng/ShortVideoProject/master/runner.sh
 
 echo  "Runner.sh下载完成"
+sleep 1
 
 echo -e "脚本结束，请输入当前ip地址加 端口号：${webport}/ShortVideoProject/index.jsp  即可访问项目"
 
+sleep 1
 echo -e "停止项目请通过JPS获取端口，通过kill -9结束进程"
 
+sleep 1
 echo -e "再次启动项目请通过runner.sh 启动项目"
 
 }
@@ -52,10 +64,11 @@ wget https://github.com/GuoJiafeng/ShortVideoProject/releases/download/1.1/short
 
 echo "SQL文件下载完成"
 
-
+sleep 1
 
 
 echo "正在创建数据库"
+sleep 1
 
 echo "请输入数据库密码"
 
@@ -64,6 +77,7 @@ mysql -uroot -p  -e "create database shortvideo"
 echo "数据库创建完成"
 
 echo "正在导入数据库"
+sleep 1
 
 echo "请输入数据库密码"
 mysql -uroot -p  -f shortvideo -e "source  $(pwd)/shortvideo.sql"
@@ -73,6 +87,36 @@ echo "导入数据完成"
 
 }
 
+
+config_github(){
+
+
+echo "151.101.72.249 http://global-ssl.fastly.net
+192.30.253.112 http://github.com
+192.30.253.119 gist.github.com
+151.101.100.133 assets-cdn.github.com
+151.101.100.133 raw.githubusercontent.com
+151.101.100.133 gist.githubusercontent.com
+151.101.100.133 cloud.githubusercontent.com
+151.101.100.133 camo.githubusercontent.com
+151.101.100.133 avatars0.githubusercontent.com
+151.101.100.133 avatars1.githubusercontent.com
+151.101.100.133 avatars2.githubusercontent.com
+151.101.100.133 avatars3.githubusercontent.com
+151.101.100.133 avatars4.githubusercontent.com
+151.101.100.133 avatars5.githubusercontent.com
+151.101.100.133 avatars6.githubusercontent.com
+151.101.100.133 avatars7.githubusercontent.com
+151.101.100.133 avatars8.githubusercontent.com
+192.168.31.170 windows10.microdone.cn " >> /etc/hosts
+
+
+service network restart
+
+
+
+
+}
 
 
 isJava() {
@@ -177,8 +221,10 @@ rpm -ivh MySQL-server-5.5.61-1.el6.x86_64.rpm
 
 
 nohup mysqld_safe --skip-grant-table &
+sleep 1
 
 echo "mysql 已经启动！，开始修改密码"
+sleep 1
 
 read -p "请输入您想要修改的密码："  mysqlpass01
 read -p "请输入ENTER继续"
