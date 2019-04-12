9a7b7d2c777072c2f5b3f0d741443918c82c8c22
GuoJiafeng <18611781163@163.com>
2019-04-12 17:19:10 +0800

    2019年4月12日17:19:03 from win

--- runner.sh
+++ runner.sh
@@ -16,7 +16,7 @@ read -p "请输入Enter确认启动项目"
 nohup java -jar ShortVideoProject.jar --spring.datasource.password=${mysqlpass} --server.port=${webport} --server.context-path=/${contextpath} &
 
 sleep 2
-echo -e "脚本结束，请输入当前ip地址加 端口号：${webport}/ShortVideoProject/index.jsp  即可访问项目"
+echo -e "脚本结束，请输入当前ip地址加 端口号：${webport}/${contextpath}/index.jsp  即可访问项目"
 
 sleep 2
 echo -e "停止项目请通过JPS获取端口，通过kill -9结束进程"
